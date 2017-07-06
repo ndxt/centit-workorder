c4dba99ff82d48aa94836d736319711377ed852a
zou_wy <zou_wy@centit.com>
2017-07-06 08:54:21 +0800

    修改

--- src/main/java/com/centit/workorder/config/SpringMvcConfig.java
+++ src/main/java/com/centit/workorder/config/SpringMvcConfig.java
@@ -1,5 +1,6 @@
 package com.centit.workorder.config;
 
+import com.centit.framework.config.WebConfig;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
@@ -10,6 +11,9 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
  */
 @Configuration
 @Import(WebConfig.class)
-@ComponentScan(basePackages = {"com.centit.framework.system.controller"})
-public class SystemSpringMvcConfig extends WebMvcConfigurerAdapter {
+@ComponentScan(basePackages = {"com.centit.workorder.controller"},
+        includeFilters = {@ComponentScan.Filter(value= org.springframework.stereotype.Controller.class)},
+        useDefaultFilters = false)
+public class SpringMvcConfig extends WebMvcConfigurerAdapter {
+
 }
