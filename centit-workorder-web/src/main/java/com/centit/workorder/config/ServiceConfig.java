2ea75a6a016dde77de7f9f4697530ff9a5bcf499
杨淮生 <codefan@centit.com>
2017-07-27 10:44:53 +0800

    代码重构

---
+++ centit-workorder-web/src/main/java/com/centit/workorder/config/ServiceConfig.java
@@ -0,0 +1,46 @@
+package com.centit.workorder.config;
+
+import com.centit.framework.hibernate.config.DataSourceConfig;
+import com.centit.framework.ip.app.config.IPAppSystemBeanConfig;
+import com.centit.framework.listener.InitialWebRuntimeEnvironment;
+import org.springframework.context.annotation.*;
+
+/**
+ * Created by codefan on 17-7-18.
+ */
+@ComponentScan(basePackages = "com.centit",
+        excludeFilters = @ComponentScan.Filter(value = org.springframework.stereotype.Controller.class))
+@Import({IPAppSystemBeanConfig.class, DataSourceConfig.class})
+@Configuration
+public class ServiceConfig {
+
+    @Bean
+    @Lazy(value = false)
+    public InitialWebRuntimeEnvironment initialEnvironment() {
+        InitialWebRuntimeEnvironment initialEnvironment = new InitialWebRuntimeEnvironment();
+        initialEnvironment.initialEnvironment();
+        return initialEnvironment;
+    }
+
+   /* @Bean
+    public NotificationCenter notificationCenter() {
+        NotificationCenterImpl notificationCenter = new NotificationCenterImpl();
+        notificationCenter.initMsgSenders();
+        //notificationCenter.registerMessageSender("innerMsg",innerMessageManager);
+        return notificationCenter;
+    }
+
+    @Bean
+    @Lazy(value = false)
+    public OperationLogWriter operationLogWriter() {
+        TextOperationLogWriterImpl operationLog = new TextOperationLogWriterImpl();
+        operationLog.init();
+        return operationLog;
+    }
+*/
+    /*@Bean
+    public InstantiationServiceBeanPostProcessor instantiationServiceBeanPostProcessor() {
+        return new InstantiationServiceBeanPostProcessor();
+    }*/
+}
+
