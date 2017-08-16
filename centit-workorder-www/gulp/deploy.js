6fb931f17177764456f4973519d92e8cf4cab1d0
zard110 <zhangk1986@gmail.com>
2017-08-16 09:38:03 +0800

    添加部署代码

--- centit-workorder-www/gulp/deploy.js
+++ centit-workorder-www/gulp/deploy.js
@@ -23,6 +23,29 @@ gulp.task('deploy', ['clean', 'clean-before-build'], function () {
     }));
 });
 
+gulp.task('deploy:all', function () {
+  return gulp.src([`${conf.paths.localWebapp}/**`])
+    .pipe(sftp({
+      host: conf.remote.host,
+      user: conf.remote.username,
+      pass: conf.remote.password,
+      remotePath: `${conf.paths.remoteWebapp}`
+    }));
+});
+
+gulp.task('deploy:war', function () {
+  const war = '/home/zk/Workspaces/java/centit-workorder/centit-workorder-web/target/workorder.war'
+  const war2 = '/home/zk/.deepinwine/Deepin-QQ/dosdevices/c:/users/zk/My Documents/Tencent Files/896974091/FileRecv/workorder.war'
+  return gulp.src([war])
+    .pipe(sftp({
+      host: conf.remote.host,
+      user: conf.remote.username,
+      pass: conf.remote.password,
+      remotePath: '/opt/websvr/tomcat8/webapps'
+    }));
+});
+
+
 gulp.task('deploy:class', function () {
   return gulp.src([`${conf.paths.localWebapp}/WEB-INF/classes/com/**`])
     .pipe(sftp({
@@ -31,6 +54,24 @@ gulp.task('deploy:class', function () {
       pass: conf.remote.password,
       remotePath: `${conf.paths.remoteWebapp}/WEB-INF/classes/com`
     }));
+});
+
+gulp.task('deploy:res', function () {
+  return gulp.src(['/D/Projects/RunData/workorder_home/**'])
+    .pipe(sftp({
+      host: conf.remote.host,
+      user: conf.remote.username,
+      pass: conf.remote.password,
+      remotePath: `/D/Projects/RunData/workorder_home`
+    }));
+});
+
+gulp.task('deploy:reset', function() {
+  return gulpSSH
+    .shell([
+      '/opt/reset.sh'
+    ], {filePath: 'reset.log'})
+    .pipe(gulp.dest('logs'));
 });
 
 gulp.task('deploy:tomcat', ['deploy:class'], function() {
