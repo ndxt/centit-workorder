22ab31638932bb81ed4661c95c8dd809faa7a991
zhang_gd <zhangguodong.1>
2017-08-22 14:21:11 +0800

    修改代码审查bug--使用logger记录异常信息，异常处理程序应保留原始异常

--- centit-workorder-module/src/main/java/com/centit/workorder/controller/FileController.java
+++ centit-workorder-module/src/main/java/com/centit/workorder/controller/FileController.java
@@ -57,7 +57,7 @@ public class FileController extends BaseController {
             downloadChineseFileName = new String(
                     HtmlUtils.htmlUnescape(paramName).getBytes("GBK"), "ISO8859-1");
         } catch (UnsupportedEncodingException e) {
-            e.printStackTrace();
+            logger.error(e.getMessage());
         }
         return downloadChineseFileName;
     }
@@ -199,6 +199,7 @@ public class FileController extends BaseController {
 
             JsonResultUtils.writeOriginalJson(json.toString(), response);
         } catch (Exception e) {
+            logger.error(e.getMessage());
             e.printStackTrace();
             JsonResultUtils.writeAjaxErrorMessage(
                     FileServerConstant.ERROR_FILE_PRETREAT,
@@ -271,6 +272,7 @@ public class FileController extends BaseController {
             }
 
         }catch (ObjectException e){
+            logger.error(e.getMessage());
             JsonResultUtils.writeAjaxErrorMessage(e.getExceptionCode(),
                     e.getMessage(), response);
         }
@@ -297,6 +299,7 @@ public class FileController extends BaseController {
             completedStoreFile(fs, fileMd5, fileSize, fileInfo.getLeft(), response);
             FileSystemOpt.deleteFile(tempFilePath);
         } catch (Exception e) {
+            logger.error(e.getMessage());
             JsonResultUtils.writeErrorMessageJson(e.getMessage(), response);
         }
     }
