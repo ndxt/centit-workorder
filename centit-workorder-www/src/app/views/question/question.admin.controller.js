b48963a94b182f8ccbd454852ef1441c31ff3b61
lsh <403048554@qq.com>
2017-10-26 19:21:00 +0800

    功能完善

--- centit-workorder-www/src/app/views/question/question.admin.controller.js
+++ centit-workorder-www/src/app/views/question/question.admin.controller.js
@@ -2,60 +2,10 @@
   'use strict'
 
   angular.module('workorder')
-    .filter('timeFilter',function () {
-      return function (inputArray, tag,times) {
-        let begTime;
-        let endTime;
-        if(tag =='TM'){
-          begTime = times.t1;
-          endTime = times.t2;
-        }
-        if(tag =='LM'){
-          begTime = times.t3;
-          endTime = times.t4;
-        }
-        if(tag =='TS'){
-          begTime = times.t5;
-          endTime = times.t6;
-        }
-        if(tag =='LS'){
-          begTime = times.t7;
-          endTime = times.t8;
-        }
-        if(tag =='TY'){
-          begTime = times.t9;
-          endTime = times.t10;
-        }
-        if(tag =='LY'){
-          begTime = times.t11;
-          endTime = times.t12;
-        }
-        begTime = new Date(begTime);
-        endTime = new Date(endTime);
-        let outArray = [];
-        if (inputArray && inputArray.length > 0) {
-          outArray = inputArray.filter(function (val) {
-            let createTime = new Date(val.createTime)
-
-            if (begTime && createTime < begTime)
-              return;
-            if (endTime && createTime > endTime)
-              return;
-            return val;
-          })
-        }
-        return outArray;
-      }
-    }).filter('pageSize', function () {
-      return function(inputArray, currentPage = 1, pageSize = 10){
-        if (!inputArray) return []
-        let start = (currentPage - 1) * pageSize
-        return inputArray.slice(start, start + pageSize)
-      }
-    }).controller('QuestionAdminController', QuestionAdminController)
+    .controller('QuestionAdminController', QuestionAdminController)
 
   /** @ngInject */
-  function QuestionAdminController($stateParams, $state,$uibModal,ConfirmModalService,DateService,toastr, QuestionAPI) {
+  function QuestionAdminController($stateParams, $state,$uibModal,$scope,ConfirmModalService,DateService,toastr, QuestionAPI) {
     let vm = this;
 
     vm.osId = $stateParams.osId;
@@ -64,6 +14,8 @@
     vm.del = del;
     vm.assign = assign;
     vm.remove = remove;
+    vm.pageChange = pageChange
+    vm.queryByTitle = queryByTitle
 
 
     vm.s_questionState = '';
@@ -137,15 +89,87 @@
 
     }
 
+
+    function del(row) {
+      ConfirmModalService.openModal('确定删除吗？')
+        .then(function () {
+          QuestionAPI.delete(Object.assign($stateParams, {questionId: row.questionId}))
+            .$promise
+            .then(function () {
+              queryQuestions(Object.assign({}, $stateParams))
+            })
+        })
+
+    }
+
     function remove(type){
-      if(type=='s_questionState')
+      if(type=='s_questionState'){
+        vm.params.questionState="";
         vm.s_questionState= '';
-      if(type=='s_questionTitle')
-        vm.s_questionTitle= '';
+      }
+      if(type=='s_questionTitle') {
+        vm.params.questionTitle="";
+        vm.s_questionTitle = '';
+        queryByTitle()
+      }
       if(type=='s_timeTag'){
+        vm.params.begin="";
+        vm.params.end="";
         vm.s_timeTag= '';
       }
     }
+
+    function pageChange(pageDesc) {
+      queryQuestions(Object.assign(vm.params,pageDesc))
+    }
+
+    function queryByTitle() {
+      vm.params.questionTitle = vm.s_questionTitle
+      queryQuestions(Object.assign(vm.params))
+    }
+    vm.params={};
+    $scope.$watchGroup(['vm.s_questionState','vm.s_timeTag'],function (newValue) {
+      vm.params = Object.assign($stateParams,
+        {
+          questionTitle:vm.s_questionTitle,
+          questionState: newValue[0],
+          pageSize:10
+        });
+      var begTime;
+      var endTime;
+      if(newValue[1]){
+        var tag = newValue[1];
+        var times = vm.getMonth;
+        if(tag =='TM'){
+          begTime = times.t1;
+          endTime = times.t2;
+        }
+        if(tag =='LM'){
+          begTime = times.t3;
+          endTime = times.t4;
+        }
+        if(tag =='TS'){
+          begTime = times.t5;
+          endTime = times.t6;
+        }
+        if(tag =='LS'){
+          begTime = times.t7;
+          endTime = times.t8;
+        }
+        if(tag =='TY'){
+          begTime = times.t9;
+          endTime = times.t10;
+        }
+        if(tag =='LY'){
+          begTime = times.t11;
+          endTime = times.t12;
+        }
+        vm.params.begin = begTime;
+        vm.params.end = endTime;
+      }
+      queryQuestions(vm.params)
+
+    })
 
   }
 })();
