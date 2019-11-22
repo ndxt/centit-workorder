ae2578331eea368a6e5b6c58861c8fa2c42c51b6
codefan <codefan@sina.com>
2019-11-22 09:32:14 +0800

    添加对Swagger的支持，并且迁移到最新的版本

--- centit-workorder-module/src/main/java/com/centit/workorder/po/HelpDoc.java
+++ centit-workorder-module/src/main/java/com/centit/workorder/po/HelpDoc.java
@@ -1,8 +1,8 @@
 package com.centit.workorder.po;
 
 import com.alibaba.fastjson.JSONObject;
-import com.centit.framework.core.po.EntityWithTimestamp;
 import com.centit.search.document.ObjectDocument;
+import com.centit.support.database.orm.GeneratorCondition;
 import com.centit.support.database.orm.GeneratorType;
 import com.centit.support.database.orm.ValueGenerator;
 import org.jsoup.helper.StringUtil;
@@ -15,85 +15,86 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 /**
- * create by scaffold 2017-05-08 
+ * create by scaffold 2017-05-08
  * @author codefan@sina.com
- 
-  系统帮助文档null   
+
+  系统帮助文档null
 */
 @Entity
 @Table(name = "F_HELP_DOC")
-public class HelpDoc implements java.io.Serializable,EntityWithTimestamp {
+public class HelpDoc implements java.io.Serializable {
 	private static final long serialVersionUID =  1L;
 
 	/**
-	 * 文档ID null 
+	 * 文档ID null
 	 */
 	@Id
 	@Column(name = "DOC_ID")
-	@GeneratedValue(generator = "assignedGenerator")
 	@ValueGenerator(strategy = GeneratorType.UUID)
 	private String docId;
 
 	/**
-	 * 类别ID null 
+	 * 类别ID null
 	 */
 	@Column(name = "CATALOG_ID")
 	//@Length(max = 32, message = "字段长度不能大于{max}")
 	private String  catalogId;
 	/**
-	 * 文档标题 null 
+	 * 文档标题 null
 	 */
 	@Column(name = "DOC_TITLE")
 	@NotBlank(message = "字段不能为空")
 	//@Length(max = 500, message = "字段长度不能大于{max}")
 	private String  docTitle;
 	/**
-	 * 文档层级 null 
+	 * 文档层级 null
 	 */
 	@Column(name = "DOC_LEVEL")
 //	@NotBlank(message = "字段不能为空")
 	private int  docLevel;
 	/**
-	 * 文档路径 null 
+	 * 文档路径 null
 	 */
 	@Column(name = "DOC_PATH")
 	@NotBlank(message = "字段不能为空")
 	//@Length(max = 500, message = "字段长度不能大于{max}")
 	private String  docPath;
 	/**
-	 * 文件ID null 
+	 * 文件ID null
 	 */
 	@Column(name = "DOC_FILE")
 	private String docFile;
 	/**
-	 * 业务系统ID null 
+	 * 业务系统ID null
 	 */
 	@Column(name = "OS_ID")
 	@NotBlank(message = "字段不能为空")
 	//@Length(max = 20, message = "字段长度不能大于{max}")
 	private String  osId;
 	/**
-	 * 业务项目模块 模块，或者表 
+	 * 业务项目模块 模块，或者表
 	 */
 	@Column(name = "OPT_ID")
 	//@Length(max = 64, message = "字段长度不能大于{max}")
 	private String  optId;
 	/**
-	 * 业务操作方法 方法，或者字段 
+	 * 业务操作方法 方法，或者字段
 	 */
 	@Column(name = "OPT_METHOD")
 	//@Length(max = 64, message = "字段长度不能大于{max}")
 	private String  optMethod;
 	/**
-	 * 编辑人员 null 
+	 * 编辑人员 null
 	 */
 	@Column(name = "UPDATE_USER")
 	//@Length(max = 32, message = "字段长度不能大于{max}")
 	private String  updateUser;
 	/**
-	 * 编辑时间 null 
+	 * 编辑时间 null
 	 */
 	@Column(name = "LAST_UPDATE_TIME")
+	@ValueGenerator(strategy = GeneratorType.FUNCTION,
+			condition = GeneratorCondition.ALWAYS, value = "today()")
 	private Date  lastUpdateTime;
 
     /**
@@ -101,7 +102,7 @@ public class HelpDoc implements java.io.Serializable,EntityWithTimestamp {
      */
 	@OneToMany(mappedBy="docId",cascade=CascadeType.ALL,fetch = FetchType.LAZY)
 	private List<HelpDocComment> helpDocComments;
-	
+
 	// Constructors
 	/** default constructor */
 	public HelpDoc() {
@@ -111,16 +112,16 @@ public class HelpDoc implements java.io.Serializable,EntityWithTimestamp {
 	public HelpDoc(String docId, String docTitle, int docLevel, String docPath, String optId) {
 		this.docId = docId;
 		this.docTitle= docTitle;
-		this.docLevel= docLevel; 
-		this.docPath= docPath; 
-		this.optId= optId; 		
+		this.docLevel= docLevel;
+		this.docPath= docPath;
+		this.optId= optId;
 	}
 
 	/** full constructor */
 	public HelpDoc(String docId, String catalogId, String docTitle, int docLevel, String docPath, String docFile,
 				   String osId, String optId, String optMethod, String updateUser, Date lastUpdateTime) {
 
-		this.docId = docId;		
+		this.docId = docId;
 		this.catalogId= catalogId;
 		this.docTitle= docTitle;
 		this.docLevel= docLevel;
@@ -130,9 +131,9 @@ public class HelpDoc implements java.io.Serializable,EntityWithTimestamp {
 		this.optId= optId;
 		this.optMethod= optMethod;
 		this.updateUser= updateUser;
-		this.lastUpdateTime= lastUpdateTime;		
+		this.lastUpdateTime= lastUpdateTime;
 	}
-	
+
 	public String getDocId() {
 		return this.docId;
 	}
@@ -141,15 +142,15 @@ public class HelpDoc implements java.io.Serializable,EntityWithTimestamp {
 		this.docId = docId;
 	}
 	// Property accessors
-  
+
 	public String getCatalogId() {
 		return this.catalogId;
 	}
-	
+
 	public void setCatalogId(String catalogId) {
 		this.catalogId = catalogId;
 	}
-  
+
 	public String getDocTitle() {
 		return this.docTitle;
 	}
@@ -157,71 +158,71 @@ public class HelpDoc implements java.io.Serializable,EntityWithTimestamp {
 	public String getLabel() {
 		return this.docTitle;
 	}
-	
+
 	public void setDocTitle(String docTitle) {
 		this.docTitle = docTitle;
 	}
-  
+
 	public int getDocLevel() {
 		return this.docLevel;
 	}
-	
+
 	public void setDocLevel(int docLevel) {
 		this.docLevel = docLevel;
 	}
-  
+
 	public String getDocPath() {
 		return this.docPath;
 	}
-	
+
 	public void setDocPath(String docPath) {
 		this.docPath = docPath;
 	}
-  
+
 	public String getDocFile() {
 		return this.docFile;
 	}
-	
+
 	public void setDocFile(String docFile) {
 		this.docFile = docFile;
 	}
-  
+
 	public String getOsId() {
 		return this.osId;
 	}
-	
+
 	public void setOsId(String osId) {
 		this.osId = osId;
 	}
-  
+
 	public String getOptId() {
 		return this.optId;
 	}
-	
+
 	public void setOptId(String optId) {
 		this.optId = optId;
 	}
-  
+
 	public String getOptMethod() {
 		return this.optMethod;
 	}
-	
+
 	public void setOptMethod(String optMethod) {
 		this.optMethod = optMethod;
 	}
-  
+
 	public String getUpdateUser() {
 		return this.updateUser;
 	}
-	
+
 	public void setUpdateUser(String updateUser) {
 		this.updateUser = updateUser;
 	}
-  
+
 	public Date getLastUpdateTime() {
 		return this.lastUpdateTime;
 	}
-	
+
 	public void setLastUpdateTime(Date lastUpdateTime) {
 		this.lastUpdateTime = lastUpdateTime;
 	}
@@ -234,18 +235,8 @@ public class HelpDoc implements java.io.Serializable,EntityWithTimestamp {
         this.helpDocComments = helpDocComments;
     }
 
-    @Override
-	public Date getLastModifyDate() {
-		return this.lastUpdateTime;
-	}
-
-	@Override
-	public void setLastModifyDate(Date date) {
-		this.lastUpdateTime = date;
-	}
-
 	public HelpDoc copy(HelpDoc other){
-  
+
 		this.setDocId(other.getDocId());
 		this.catalogId = other.getCatalogId();
 		this.docTitle = other.getDocTitle();
@@ -257,52 +248,52 @@ public class HelpDoc implements java.io.Serializable,EntityWithTimestamp {
 		this.optMethod = other.getOptMethod();
 		this.updateUser = other.getUpdateUser();
 		this.lastUpdateTime = other.getLastUpdateTime();
-	
+
 		return this;
 	}
-	
+
 	public HelpDoc copyNotNullProperty(HelpDoc other){
-  
+
 		if( other.getDocId() != null)
 			this.setDocId(other.getDocId());
-  
+
 		if( other.getCatalogId() != null)
 			this.catalogId= other.getCatalogId();
 		if( other.getDocTitle() != null)
-			this.docTitle= other.getDocTitle();  
+			this.docTitle= other.getDocTitle();
 		if( other.getDocLevel() != -1)
-			this.docLevel= other.getDocLevel();  
+			this.docLevel= other.getDocLevel();
 		if( other.getDocPath() != null)
-			this.docPath= other.getDocPath();  
+			this.docPath= other.getDocPath();
 		if( other.getDocFile() != null)
-			this.docFile= other.getDocFile();  
+			this.docFile= other.getDocFile();
 		if( other.getOsId() != null)
-			this.osId= other.getOsId();  
+			this.osId= other.getOsId();
 		if( other.getOptId() != null)
-			this.optId= other.getOptId();  
+			this.optId= other.getOptId();
 		if( other.getOptMethod() != null)
-			this.optMethod= other.getOptMethod();  
+			this.optMethod= other.getOptMethod();
 		if( other.getUpdateUser() != null)
-			this.updateUser= other.getUpdateUser();  
+			this.updateUser= other.getUpdateUser();
 		if( other.getLastUpdateTime() != null)
-			this.lastUpdateTime= other.getLastUpdateTime();		
+			this.lastUpdateTime= other.getLastUpdateTime();
 
 		return this;
 	}
 
 	public HelpDoc clearProperties(){
-  
+
 		this.catalogId= null;
-		this.docTitle= null;  
+		this.docTitle= null;
 		this.docLevel= -1;
-		this.docPath= null;  
-		this.docFile= null;  
-		this.osId= null;  
-		this.optId= null;  
-		this.optMethod= null;  
-		this.updateUser= null;  
+		this.docPath= null;
+		this.docFile= null;
+		this.osId= null;
+		this.optId= null;
+		this.optMethod= null;
+		this.updateUser= null;
 		this.lastUpdateTime= null;
-	
+
 		return this;
 	}
 
